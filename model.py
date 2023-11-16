d21283da11bbbe883e2d284ef7082f5ab2938aa9
Aditya S <aditya24102001@gmail.com>
2023-11-16 11:50:34 +0000

    Improve generator architecture with batch normalisation and dimension size.

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, Dense, Flatten, Reshape, LeakyReLU, Dropout, UpSampling2D
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, Reshape, LeakyReLU, Dropout, UpSampling2D, BatchNormalization
 
 load_directory = 'preprocessed_data'
 image_shape = (128, 28, 28, 1)
@@ -14,33 +14,37 @@ ds = tf.data.experimental.load(load_directory, dataset_structure)
 def build_generator():
     model = Sequential()
 
-    model.add(Dense(7 * 7 * 128, input_dim=128))
-    model.add(LeakyReLU(0.2))
-    model.add(Reshape((7, 7, 128)))
+    # Fully connected layer
+    model.add(Dense(7 * 7 * 256, input_dim=128))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Reshape((7, 7, 256)))
+
+    # Upsampling and Convolutional Blocks
+    model.add(UpSampling2D())
+    model.add(Conv2D(256, kernel_size=3, padding='same'))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(LeakyReLU(alpha=0.2))
 
     model.add(UpSampling2D())
-    model.add(Conv2D(128, 5, padding='same'))
-    model.add(LeakyReLU(0.2))
+    model.add(Conv2D(128, kernel_size=3, padding='same'))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(LeakyReLU(alpha=0.2))
 
-    model.add(UpSampling2D())
-    model.add(Conv2D(128, 5, padding='same'))
-    model.add(LeakyReLU(0.2))
+    model.add(Conv2D(64, kernel_size=3, padding='same'))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(LeakyReLU(alpha=0.2))
 
-    model.add(Conv2D(128, 4, padding='same'))
-    model.add(LeakyReLU(0.2))
-
-    model.add(Conv2D(128, 4, padding='same'))
-    model.add(LeakyReLU(0.2))
-
-    model.add(Conv2D(1, 4, padding='same', activation='sigmoid'))
+    model.add(Conv2D(1, kernel_size=3, padding='same', activation='sigmoid'))
 
     return model
 
 generator = build_generator()
 generator.summary()
 
-img = generator.predict(np.random.randn(4, 128, 1))
+# Generate 4 random noise samples and use the generator to create images
+img = generator.predict(np.random.randn(4, 128))
 fig, ax = plt.subplots(ncols=4, figsize=(20, 20))
 for idx, img in enumerate(img):
     ax[idx].imshow(np.squeeze(img)) # Plot the image using a specific subplot
     ax[idx].title.set_text(idx) # Appending the image label as the plot title
+plt.show()
