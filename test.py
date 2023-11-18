346dc9595e8417b81e7bae3490e61ddb45d72f5e
Aditya S <aditya24102001@gmail.com>
2023-11-18 14:38:34 +0000

    Added comments describing image generation loop.

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
-from model import generator
+from model import build_generator
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import os
 
+generator = build_generator()
 generator.load_weights(os.path.join('models', 'generatormodel.h5'))
-imgs = generator.predict(tf.random.normal((16, 128, 1)))
+imgs = generator.predict(tf.random.normal((16, 128, 1))) # Generating 16 images; The generator takes a random noise vector as input.
 
+# Plot the generated images in a 4x4 grid
 fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(10,10))
 for r in range(4):
     for c in range(4):
+        # Displaying the image at position [r, c] in the grid
+        # The index for the image is calculated based on the current row and column
         ax[r][c].imshow(imgs[(r+1)*(c+1)-1])
+plt.show()
